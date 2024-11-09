454de437d7d192fe3dff9d8afaf3e87c9a94ff0a
Ivy-kalamansi19 <aguasi372@gmail.com>
2024-11-09 19:21:30 +0800

    Update leave.js

--- script/event/leave.js
+++ script/event/leave.js
@@ -1,4 +1,4 @@
 module.exports = function (event) {
   const { threadID } = event;
-  api.sendMessage("Goodbye! See you next time!", threadID);
+  api.sendMessage("𝗚𝗼𝗼𝗱𝗯𝘆𝗲! 𝗦𝗲𝗲 𝘆𝗼𝘂 𝗻𝗲𝘅𝘁 𝘁𝗶𝗺𝗲!🪦🕊️", threadID);
 };
